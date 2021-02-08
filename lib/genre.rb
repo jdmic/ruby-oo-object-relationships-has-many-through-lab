29c0320255a5f7112d6fbb1c5a022f0d9c45beb3
James <james.michalec58@gmail.com>
2021-02-08 10:00:38 -0800

    commit tree 7280714a0abe749b804831bbe14126f4a5a3af71

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,33 @@
+require "pry"
+class Genre
 
+    @@all = []
+
+    attr_accessor 
+
+    def initialize(genre)
+        @genre=genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.genre == self
+        end
+    end
+
+    def name
+        @genre
+    end
+
+    def artists
+        songs.map do |song|
+            song.artist
+        end
+    end
+
+end
